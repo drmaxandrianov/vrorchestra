c7ab22784991fcffc2521108c479e1b3c457bb43
Maxim Pestun <max.pestun@gmail.com>
2011-10-18 20:14:44 +0400

    Creating all needed folders and home page.

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html>
+	<head>
+		<title>VR Orchestra</title>
+		<!--[if lt IE 9]>
+			<script>
+				var e = ("article,aside,figcaption,figure,footer,header,hgroup,nav,section,time").split(',');
+				for (var i = 0; i < e.length; i++) {
+					document.createElement(e[i]);
+				}
+			</script>
+		<![endif]-->
+	</head>
+	<body>
+		<header>
+			<h1>
+				Welcome to the VR Orchestra.
+			</h1>
+		</header>
+		<section>
+			<p>
+				This site is under construction. Come back later, please. For now you can
+				visit the project home page at <a href="http://vrorchestra.weebly.com/">vrorchestra.weebly.com</a>.
+				Thank you for attantion!
+			</p>
+		</section>
+		<footer>
+			<p>
+				Best Regards,<br />
+				Maxim Pestun,<br />
+				Architect.<br />
+			</p>
+		</footer>
+		<script type="text/javascript">
+		</script>
+	</body>
+</html>
